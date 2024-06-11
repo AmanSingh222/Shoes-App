05dd2c23694bafe9f12e33910a7e0b9938a62366
Aman <93045355+AmanSingh222@users.noreply.github.com>
2024-06-11 19:04:21 +0530

    First Commit

--- app/src/main/java/com/example/shoesapp/Screens/ProductDetailScreen.kt
+++ app/src/main/java/com/example/shoesapp/Screens/ProductDetailScreen.kt
@@ -2,13 +2,22 @@ package com.example.shoesapp.Screens
 
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowLeft
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,6 +27,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.DefaultShadowColor
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -57,11 +72,25 @@ fun ProductDetailScreen(productId: String = "1") {
     }
 
 
+    var productScale by remember {
+        mutableStateOf(0.6f)
+    }
+    var productRotate by remember {
+        mutableStateOf(-60f)
+    }
+
+    val animationProductState = animateFloatAsState(targetValue = productScale)
+    val animationProductRotate = animateFloatAsState(targetValue = productRotate)
+
+
+
+
 
     LaunchedEffect(true) {
         delay(150)
-        xOffset=140.dp
-        yOffset= (-130).dp
+        xOffset = 140.dp
+        yOffset = (-130).dp
+        productScale = 1f
 
     }
 
@@ -83,10 +112,36 @@ fun ProductDetailScreen(productId: String = "1") {
                     shape = CircleShape
                 )
 
+        )
+
+        //this is back button
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .padding(start = 16.dp, top = 16.dp)
+                .shadow(
+                    elevation = 24.dp, spotColor = DefaultShadowColor,
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .background(color = Color.White, shape = RoundedCornerShape(22.dp))
+                .size(36.dp)
         ) {
 
+            Icon(imageVector = Icons.Rounded.KeyboardArrowLeft, contentDescription = null)
 
         }
+
+        //column for product image
+        Column(
+            modifier = Modifier
+                .scale(animationProductState.value)
+                .rotate(animationProductRotate.value)
+                .padding(48.dp, 30.dp)
+        ) {
+            Image(painter = painterResource(id = product.imageRes), contentDescription = null)
+        }
+
+
     }
 
 }
